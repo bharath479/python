316d8efdf699ab558b7238ae57d7cb0d1903bfc1
bharath479 <53123576+bharath479@users.noreply.github.com>
2026-06-19 10:56:07 +0000

    Fix print statement and add password variable
    Fixed syntax error in print statement and added a password variable.

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,8 +10,8 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" )
     }
 }
-
+Password = "bharath@123"
 //foo
