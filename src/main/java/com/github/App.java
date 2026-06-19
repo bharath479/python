327690e41edb70c2d327e11f1ab3faa1781aa462
bharath479 <53123576+bharath479@users.noreply.github.com>
2026-06-19 10:57:45 +0000

    Fix syntax error in App.java

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,8 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World!" )
+        System.out.println( "Hello World!"
     }
-}
-Password = "bharath@123"
+
 //foo
