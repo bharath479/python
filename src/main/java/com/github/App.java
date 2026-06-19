12fd5303bc1be4eb1c57b839e1196621a4c40d3c
bharath479 <53123576+bharath479@users.noreply.github.com>
2026-06-19 11:39:31 +0000

    Update App.java

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -7,7 +7,7 @@ package com.github;
  */
 public class App 
 {
-    public static void main( String[] args'
+    public static void main( String[] args'ds
     {
     	//Comment
         System.out.println( "Hello World!"
